6c621fbf979e647f983d3843e75f5738100751bd
Kenyo Saenz Rojas <saenzkenyo@gmail.com>
2025-10-31 18:09:01 -0500

    feat(properties): agregar fucnionalidad para listar propiedades en el frontend

--- src/components/Datos.jsx
+++ src/components/Datos.jsx
@@ -1,47 +1,89 @@
-import React from 'react'
+// import { useEffect,useState } from "react";
 
-const Datos = () => {
- 
-  const ApiList='http://localhost:8080/api/propiedades/listar'
-  fetch(ApiList)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    // .catch((error) => console.error('Error fetching data:', error));
-  
-  return (
-    
+// const Datos = () => {
+//   const [propiedades, setPropiedades] = useState([]);
 
-    <div class='flex gap-6 flex-col md:gap-3'>
-      <div class='relative overflow-hidden h-40 bg-muted'>
-        <span class='absolute py-0.1 left-70 top-1 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden border-transparent [a&]:hover:bg-primary/90 bg-white/90 z-10 text-foreground md:left-3 sm:left-3 2xl:left-3 lg:left-3 xl:left-3'>Disponible</span>
-        <img class='sm:w-full object-cover group-hover:scale-105 transition-transform duration-300 z-0  h-[192px] w-[382px] inset-0 text-transparent' src="/public/img/fondo.png" alt="" />
-      </div>
-      <div class='p-[16px] flex flex-col gap-6'>
-         <div class='flex gap-2 items-start justify-between'>
-          <h3 class='text-[64px]'>Terreno Residencial en San Blas</h3>
-          <span class='py-0.3 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden text-foreground :hover:text-accent-foreground shrink-0'>Residencial</span>
-         </div>
-         <div>
-          <p class=' text-sm'>Hermoso terreno ubicado en el tradicional barrio de San Blas, con vista panorámica de la ciudad de.</p>
-         </div>
-         <div class='flex gap-6 flex-col'>
-          <div class='flex gap-6'>
-            <img class='w-5 text-xs' src="/public/img/MapPin.svg" alt="" />
-            <p>Calle Tandapata 450, San Blas</p>
-          </div>
-          <div class='flex gap-6'>
-            <img class='w-3 text-xs' src="/public/img/arrow.png" alt="" />
-            <p class=''>350 m²</p>
-          </div>
-         </div>
-         
-      </div>
-      <div class='flex gap-6 px-4 pb-4'>
-          <svg  class='text-amber-950 ' viewbox="0 0 32 32" width="32" height="32" stroke="currentColor" fill="brown"><path d="M8 21.6c0.3 3.3 3 5.7 7.2 6V30h2.1v-2.4c4.5-0.4 7.4-2.9 7.3-6.6 0-3.2-1.9-5-5.9-6.1l-1.4-0.4V6.9c2.2 0.2 3.8 1.4 4.1 3.3h2.9c-0.3-3.2-3.1-5.5-7-5.8V2H15.2v2.5c-3.9 0.5-6.5 2.9-6.6 6.3 0 2.9 1.9 5 5.4 5.8l1.2 0.3v8.1c-2.3-0.3-3.9-1.6-4.3-3.4H8z m6.8-7.7c-2.1-0.5-3.2-1.6-3.2-3.2 0-1.9 1.4-3.3 3.6-3.7v7l-0.4-0.1z m3.2 3.7c2.6 0.6 3.7 1.7 3.7 3.6 0 2.2-1.7 3.7-4.4 3.8V17.5l0.7 0.1z"  /></svg>
-          <p class='text-2xl text-amber-950 font-bold'>USD 250,000</p>
-         </div>
-    </div>
-  )
-}
+//   const ApiList = "http://localhost:8080/api/propiedades/listar";
 
-export default Datos
+//   async function obtenerUsuarios() {
+//     try {
+//       const respuesta = await fetch(ApiList);
+//       const datos = await respuesta.json();
+//       setPropiedades(datos);
+//       console.log("prop",setPropiedades)
+//       console.log(datos.length)
+      
+//     } catch (error) {
+//       console.error("❌ Error al obtener la API:", error);
+//     }
+//   }
+
+//   useEffect(() => {
+//     obtenerUsuarios();
+//   }, []);
+
+//   return (
+//     // grid gap-6 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 
+//     // class="bg-white h-[523px] w-[360px] 2xl:w-[365px] xl:w-[300px] lg:w-[330px]  md:w-[340px] bg-card text-card-foreground flex flex-col gap-3 py-6 rounded-xl border shadow-sm overflow-hidden hover:shadow-lg transition-shadow cursor-pointer group sm:w-[100%]"
+//     <ul class="grid gap-6 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
+//       {propiedades.map((prop) => (
+//         <li key={prop.id}>
+//           <div className="flex gap-6 flex-col md:gap-3">
+//              <div className="relative overflow-hidden h-40 bg-muted">  {/**/}
+//               <span className="absolute py-0.1 left-70 top-1 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden border-transparent [a&]:hover:bg-primary/90 bg-white/90 z-10 text-foreground md:left-3 sm:left-3 2xl:left-3 lg:left-3 xl:left-3">
+//                 {prop.estado}
+//               </span>
+//               <img
+//                 className="sm:w-full object-cover group-hover:scale-105 transition-transform duration-300 z-0  h-[192px] w-[382px] inset-0 text-transparent"
+//                 src="/public/img/fondo.png"
+//                 alt=""
+//               />
+//             </div>
+//             <div className="p-[16px] flex flex-col gap-6">
+//               <div className="flex gap-2 items-start justify-between">
+//                 <h3 className="text-[64px]">{prop.titulo}</h3>
+//                 <span className="py-0.3 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden text-foreground :hover:text-accent-foreground shrink-0">
+//                   {prop.tipo}
+//                 </span>
+//               </div>
+//               <div>
+//                 <p className=" text-sm">
+//                   {prop.descripcion}
+//                 </p>
+//               </div>
+//               <div className="flex gap-6 flex-col">
+//                 <div className="flex gap-6">
+//                   <img
+//                     className="w-5 text-xs"
+//                     src="/public/img/MapPin.svg"
+//                     alt=""
+//                   />
+//                   <p>{prop.direccion}</p>
+//                 </div>
+//                 <div className="flex gap-6">
+//                   <img className="w-3 text-xs" src="/public/img/arrow.png" alt="" />
+//                   <p className="">{prop.metrosCuadrados} m²</p>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="flex gap-6 px-4 pb-4">
+//               <svg
+//                 className="text-amber-950 "
+//                 viewbox="0 0 32 32"
+//                 width="32"
+//                 height="32"
+//                 stroke="currentColor"
+//                 fill="brown"
+//               >
+//                 <path d="M8 21.6c0.3 3.3 3 5.7 7.2 6V30h2.1v-2.4c4.5-0.4 7.4-2.9 7.3-6.6 0-3.2-1.9-5-5.9-6.1l-1.4-0.4V6.9c2.2 0.2 3.8 1.4 4.1 3.3h2.9c-0.3-3.2-3.1-5.5-7-5.8V2H15.2v2.5c-3.9 0.5-6.5 2.9-6.6 6.3 0 2.9 1.9 5 5.4 5.8l1.2 0.3v8.1c-2.3-0.3-3.9-1.6-4.3-3.4H8z m6.8-7.7c-2.1-0.5-3.2-1.6-3.2-3.2 0-1.9 1.4-3.3 3.6-3.7v7l-0.4-0.1z m3.2 3.7c2.6 0.6 3.7 1.7 3.7 3.6 0 2.2-1.7 3.7-4.4 3.8V17.5l0.7 0.1z" />
+//               </svg>
+//               <p className="text-2xl text-amber-950 font-bold">{prop.precio}</p>
+//             </div>
+//           </div>
+//         // </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default Datos
--- src/components/Terreno.jsx
+++ src/components/Terreno.jsx
@@ -1,18 +1,30 @@
-
-import Datos from './Datos'
+import { useEffect,useState } from "react";
+// import Datos from './Datos'
 
 const Terreno = () => {
+  const [propiedades, setPropiedades] = useState([]);
 
-    // const terreno=document.getElementById('btnTerreno');
-    // terreno.addEventListener('click',()=>{
-    //   console.log('click');
-    //   terreno.innerHTML=`<li>hola</li>`
-    // })
+  const ApiList = "http://localhost:8080/api/propiedades/listar";
 
-  // flex gap-6 w-full justify-center
+  async function obtenerUsuarios() {
+    try {
+      const respuesta = await fetch(ApiList);
+      const datos = await respuesta.json();
+      setPropiedades(datos);
+      console.log("prop",setPropiedades)
+      console.log(datos.length)
+      
+    } catch (error) {
+      console.error("❌ Error al obtener la API:", error);
+    }
+  }
+  // se ejecuta una sola vez cuando el componente se monta
+  useEffect(() => {
+    obtenerUsuarios();
+  }, []);
+
+
   return (
-    
-
     <div class="grid gap-6 bg-[#FEF7F2] 2xl:w-[1440px] 2xl:grid-cols-[280px_1fr] 2xl:gap-6 xl:grid-cols-[280px_1fr] xl:px-0 lg:px-0  2xl:px-0 lg:grid-cols-[280px_1fr] md:grid-cols-1 sm:grid-cols-1 sm:px-5 md:w-[740px] sm:w-[590px] lg:w-full">
       <section class="sticky space-y-4 bg-card rounded-lg border md:px-0 sm:px-0 2xl:px-0 xl:px-0 w-full lg:px-0">
         <div class=" w-full md:w-[100%] sm:w-[100%] xs:w-[100%] 2xl:w-[280px] lg:w-[280px] bg-white rounded-xl border p-4 shadow-sm overflow-hidden hover:shadow-lg transition-shadow flex flex-col gad-4 ">
@@ -24,11 +36,26 @@ const Terreno = () => {
             <div class="pt-2">
               <select
                 id="opciones"
-                class="rounded-sm w-20 flex justify-center h-6 gap-1 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input  min-w-0 border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-1 file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-md file:font-medium ">
-                <option value="" >Todos
-                    <svg class='w-4' data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" ></path>
-                    </svg>
+                class="rounded-sm w-20 flex justify-center h-6 gap-1 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input  min-w-0 border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-1 file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-md file:font-medium "
+              >
+                <option value="">
+                  Todos
+                  <svg
+                    class="w-4"
+                    data-slot="icon"
+                    fill="none"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    ></path>
+                  </svg>
                 </option>
                 <option value="opcion1">Residencia</option>
                 <option value="opcion2">Comercial</option>
@@ -49,20 +76,34 @@ const Terreno = () => {
           <div class="pb-4">
             <label for="opciones">Estados </label>
             <div class="pt-2">
-              <button type='button'>
+              <button type="button">
                 <select
-                id="opciones"
-                class="rounded-sm w-20 flex justify-center h-6 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input  min-w-0 border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-1 file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-md file:font-medium ">
-                <option value="" class="p-2">
+                  id="opciones"
+                  class="rounded-sm w-20 flex justify-center h-6 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input  min-w-0 border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-1 file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-md file:font-medium "
+                >
+                  <option value="" class="p-2">
                     Todos
-                    <svg class='w-4' data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" ></path>
+                    <svg
+                      class="w-4"
+                      data-slot="icon"
+                      fill="none"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                      ></path>
                     </svg>
-                </option>
-                <option value="opcion1">Disponible</option>
-                <option value="opcion2">Reservado</option>
-                <option value="opcion3">Vendido</option>
-              </select>
+                  </option>
+                  <option value="opcion1">Disponible</option>
+                  <option value="opcion2">Reservado</option>
+                  <option value="opcion3">Vendido</option>
+                </select>
               </button>
             </div>
             {/* <label htmlFor="">Estados</label>
@@ -141,16 +182,80 @@ const Terreno = () => {
 
       <section class="sm:px-0 xl:gap-6 2xl:gap-5 md:px-0 ">
         <p class="w-full pb-5">6 terrenos encontrados</p>
+
         <div class="grid sm:grid-cols-1 md:grid-cols-2 xl:gap-6 gap-6 2xl:grid-cols-3 xl:grid-cols-3 2xl:gap-6 lg:grid-cols-2 lg:gap-6 md:gap-6 justify-center ">
-          <div class="bg-white h-[523px] w-[360px] 2xl:w-[365px] xl:w-[300px] lg:w-[330px]  md:w-[340px] bg-card text-card-foreground flex flex-col gap-3 py-6 rounded-xl border shadow-sm overflow-hidden hover:shadow-lg transition-shadow cursor-pointer group sm:w-[100%]">
-            <Datos />
-          </div>
-          <div class="bg-white h-[523px]  w-[360px] 2xl:w-[365px] xl:w-[300px] lg:w-[330px] md:w-[340px]  bg-card text-card-foreground flex flex-col gap-3 py-6 rounded-xl border  shadow-sm overflow-hidden hover:shadow-lg transition-shadow cursor-pointer group sm:w-[100%]">
-            <Datos />
-          </div>
-          <div class="bg-white h-[523px]  w-[360px] 2xl:w-[365px] xl:w-[300px] lg:w-[330px]  md:w-[340px] sm:w-[100%] bg-card text-card-foreground flex flex-col gap-3 py-6 rounded-xl border  shadow-sm overflow-hidden hover:shadow-lg transition-shadow cursor-pointer group">
-            <Datos />
-          </div>
+          {propiedades.map((prop) => (
+            <div
+              key={prop.id}
+              class="bg-white h-[523px] w-[360px] 2xl:w-[365px] xl:w-[300px] lg:w-[330px]  md:w-[340px] bg-card text-card-foreground flex flex-col gap-3 py-6 rounded-xl border shadow-sm overflow-hidden hover:shadow-lg transition-shadow cursor-pointer group sm:w-[100%]"
+            >
+              <div className="flex gap-6 flex-col md:gap-3">
+                <div className="relative overflow-hidden h-40 bg-muted">
+                  {" "}
+                  {/**/}
+                  <span className="absolute py-0.1 left-70 top-1 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden border-transparent [a&]:hover:bg-primary/90 bg-white/90 z-10 text-foreground md:left-3 sm:left-3 2xl:left-3 lg:left-3 xl:left-3">
+                    {prop.estado}
+                  </span>
+                  <img
+                    className="sm:w-full object-cover group-hover:scale-105 transition-transform duration-300 z-0  h-[192px] w-[382px] inset-0 text-transparent"
+                    src="/public/img/fondo.png"
+                    alt=""
+                  />
+                </div>
+                <div className="p-[16px] flex flex-col gap-6">
+                  <div className="flex gap-2 items-start justify-between">
+                    <h3 className="text-[64px]">{prop.titulo}</h3>
+                    <span className="py-0.3 inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden text-foreground :hover:text-accent-foreground shrink-0">
+                      {prop.tipo}
+                    </span>
+                  </div>
+                  <div>
+                    {/* <p className=" text-sm">
+                  {prop.descripcion}
+                </p> */}
+                    <p className="text-sm">
+                      {prop.descripcion.length > 92
+                        ? prop.descripcion.substring(0, 92) + "..."
+                        : prop.descripcion}
+                    </p>
+                  </div>
+                  <div className="flex gap-6 flex-col">
+                    <div className="flex gap-6">
+                      <img
+                        className="w-5 text-xs"
+                        src="/public/img/MapPin.svg"
+                        alt=""
+                      />
+                      <p>{prop.direccion}</p>
+                    </div>
+                    <div className="flex gap-6">
+                      <img
+                        className="w-3 text-xs"
+                        src="/public/img/arrow.png"
+                        alt=""
+                      />
+                      <p className="">{prop.metrosCuadrados} m²</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-6 px-4 pb-4">
+                  <svg
+                    className="text-amber-950 "
+                    viewbox="0 0 32 32"
+                    width="32"
+                    height="32"
+                    stroke="currentColor"
+                    fill="brown"
+                  >
+                    <path d="M8 21.6c0.3 3.3 3 5.7 7.2 6V30h2.1v-2.4c4.5-0.4 7.4-2.9 7.3-6.6 0-3.2-1.9-5-5.9-6.1l-1.4-0.4V6.9c2.2 0.2 3.8 1.4 4.1 3.3h2.9c-0.3-3.2-3.1-5.5-7-5.8V2H15.2v2.5c-3.9 0.5-6.5 2.9-6.6 6.3 0 2.9 1.9 5 5.4 5.8l1.2 0.3v8.1c-2.3-0.3-3.9-1.6-4.3-3.4H8z m6.8-7.7c-2.1-0.5-3.2-1.6-3.2-3.2 0-1.9 1.4-3.3 3.6-3.7v7l-0.4-0.1z m3.2 3.7c2.6 0.6 3.7 1.7 3.7 3.6 0 2.2-1.7 3.7-4.4 3.8V17.5l0.7 0.1z" />
+                  </svg>
+                  <p className="text-2xl text-amber-950 font-bold">
+                    {prop.precio}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </div>
