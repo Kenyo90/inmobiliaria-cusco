7760135f2e49a3cd5993417dd41114ff7bbd1a58
Kenyo Saenz Rojas <saenzkenyo@gmail.com>
2025-11-30 01:39:46 -0500

    (feat) Funcion a los iconos de las redes sociales (footer)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@ import { FiFacebook } from "react-icons/fi";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FiInstagram } from "react-icons/fi";
 import { SiTiktok } from "react-icons/si";
+import { RiYoutubeLine } from "react-icons/ri";
 import { BsTwitterX } from "react-icons/bs";
-import { FiLinkedin } from "react-icons/fi";
+import { PiTiktokLogo } from "react-icons/pi";
 const Footer = () => {
   return (
     <footer class="w-full">
-      <div class="mt-10 flex flex-col pl-10 justify-around py-6 bg-orange-300 gap-8 2xl:flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-col sm:pl-10 sm:px-[16px] sm:py-[48px]">
+      <div class="mt-10 flex flex-col pl-10 justify-around py-6 bg-orange-300 gap-8 2xl:flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col sm:pl-10 sm:px-[16px] sm:py-[48px]">
         <div>
           <h3 class='text-black !text-[16px] !font-[650] pb-[10px]'>Inmobiliaria Cusco</h3>
           <p>Tu socio inmobiliario de confianza en Cusco</p>
@@ -25,12 +26,13 @@ const Footer = () => {
         </div>
         <div class='flex flex-col gap-3'>
           <h3 class='text-black !text-[16px] !font-[650] pb-[10px]'>Síguenos en Redes Sociales</h3>
-          <div class='flex  gap-3 w-10'>
+          <div class='flex  gap-5 w-10'>
             {/* <span><FaSquareFacebook/></span> */}
-            <span class='size-5'><FiFacebook/></span>
-            <span><FiInstagram/></span>
-            <span><BsTwitterX/></span>
-            <span><FiLinkedin/></span>
+            <a class='p-2 cursor-pointer' href="https://www.facebook.com/Ayllulagunahuaypo" target="_blank"><FiFacebook/></a>
+            <a class='p-2 cursor-pointer' href="https://www.instagram.com/ayllulagunahuaypo/?igsh=MWw5a2oycThoczFwag%3D%3D#" target="_blank"><FiInstagram/></a>
+            {/* <a class='p-2 cursor-pointer' href="" target="_blank"><BsTwitterX/></a> */}
+            <a class='p-2 cursor-pointer' href="https://www.tiktok.com/@cusicoyllur_chinchero?_r=1&_t=ZS-91p9gJyLzMI" target="_blank"><PiTiktokLogo/></a>
+            <a class='p-2 cursor-pointer' href="https://youtube.com/watch?v=qWZNLsqJZTs&feature=shared" target="_blank"><RiYoutubeLine/></a>
           </div>
         </div>
       </div>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -65,8 +65,8 @@ const Inicio = () => {
         </section>
         
           <div class=" fixed  bottom-5 right-5 ">
-          {/* https://wa.me/+51962786293?text=Hola%20quiero%20contactame%20contigo */}
-          <a class="!hover:bg-amber-400" href="https://wa.me/+51962786293" target="_blank">
+          {/* https://wa.me/+51919527727  */}
+          <a class="!hover:bg-amber-400" href="https://wa.me/+51919527727?text=Hola%20quiero%20contactame%20contigo" target="_blank">
             <img class="w-13 hover:w-15 transition-all duration-200"
               src="../../public/img/whatsapp.png" alt="" />
           </a>
@@ -79,69 +79,3 @@ const Inicio = () => {
 };
 
 export default Inicio;
-
-// import { useState } from "react";
-// import Busqueda from "../components/Busqueda";
-// import Footer from "../components/Footer";
-// import Formulario from "../components/Formulario";
-// import Navbar from "../components/Navbar";
-// import Terreno from "../components/Terreno";
-
-// const Inicio = () => {
-//   const [busqueda, setBusqueda] = useState("");
-
-//   // bg-[url(/public/img/fondo.png)] bg-cover bg-center bg-no-repeat h-fit
-//   return (
-//     <div class="m-0 box-border text-base bg-[#FEF7F2] h-fit">
-//       <header class="justify-center flex items-center w-full 2xl:w-full xl:w-[100%] lg:w-[100%] md:w-[100%] sm:w-[100%]">
-//         <Navbar />
-//       </header>
-//       <main id="Propiedades" class="flex justify-center bg-[#FEF7F2] h-fit">
-//         <section class=" py-[32px] max-w-[1468px] h-[100%] bg-[#FEF7F2] my-auto flex justify-center flex-col xl:px-[64px] xl:mx-[20px] lg:px-[16px] lg:mx-[20px] 2xl:w-full 2xl:justify-center md:px-[35px] md:mx-[35px] md:items-center sm:px-[35px] sm:mx-[35px] 2xl:mx-auto">
-//           <div class="max-w-[100%] w-[400px] mx-auto max-h-[48px] flex justify-center py-1 mb-10 items-center 2xl:px-0  xl:w-[100%] xl:px-0 md:px-0 md:w-[100%] lg:w-full bg-[#FEF7F2] lg:px-0 2xl:w-full  sm:w-full">
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               class="h-10 w-10 text-[#8a3a13] mr-2"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="currentColor"
-//             >
-//               <path
-//                 stroke-linecap="round"
-//                 stroke-linejoin="round"
-//                 stroke-width="2"
-//                 d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
-//               />
-//             </svg>
-//             <input
-//               type="text"
-//               placeholder="Buscar por ubicación, distrito o características..."
-//               value={busqueda.busqueda}
-//               onChange={(e) => setBusqueda("busqueda",e.target.value)}
-//               class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input w-full min-w-0 rounded-md border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-1 file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-md file:font-medium disabled:pointer-events-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-[#8a3a13] focus-visible:ring-[#8a3a13]/50 focus-visible:ring-[5px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive !pl-5 !h-10 text-base"
-//             />  
-//           </div>
-//           <Terreno />
-//           <Formulario/>
-//           <Footer/>
-//         </section>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Inicio;
- {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 w-10 text-[#8a3a13] mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
-              />
-            </svg> */}
