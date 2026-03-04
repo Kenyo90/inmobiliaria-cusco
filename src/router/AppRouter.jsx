3f664203ad5ce3ddcdbe1dff2157c7ed58fe5769
Kenyo Saenz Rojas <saenzkenyo@gmail.com>
2026-03-03 23:48:26 -0500

    Primer commit

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import Inicio from '../pages/Inicio'
-import Dashboard from "../pages/Dashboard"
-import Crud from "../pages/Crud"
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route>
         <Route path='/' element={<Inicio/>}></Route>
-        <Route path='/admin' element={<Dashboard/>}></Route>
-        <Route path='/Crud' element={<Crud/>}></Route>
+
       </Route>
     </Routes>
   )
